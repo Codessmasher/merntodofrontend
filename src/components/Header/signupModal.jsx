67ad9b0b81d949a4e8af2e1cc3161a1a5e71a3b1
DEEPJYOTI DAS <95518873+Codessmasher@users.noreply.github.com>
2024-03-06 02:22:59 +0530

    Add files via upload

--- src/components/Header/signupModal.jsx
+++ src/components/Header/signupModal.jsx
@@ -35,7 +35,7 @@ export default function SignupModal({ open, handleOpen, handleClose }) {
     }
 
     // Send the form data to the Express server
-    axios.post("https://merntodofrontend-rosy.vercel.app/api/users/signup",formData) 
+    axios.post("http://127.0.0.1:5000/api/users/signup",formData) 
       .then((res) => {
         // Handle the response from the server, if needed
         // if(!isEmail(formData.email)){
