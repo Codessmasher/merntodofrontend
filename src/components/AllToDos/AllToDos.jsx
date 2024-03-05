3bf502b5b1d4ca6a2ad519f7626b023d09dde5e7
DEEPJYOTI DAS <95518873+Codessmasher@users.noreply.github.com>
2024-03-06 02:25:49 +0530

    Update AllToDos.jsx

--- src/components/AllToDos/AllToDos.jsx
+++ src/components/AllToDos/AllToDos.jsx
@@ -17,7 +17,7 @@ const AllToDos = () => {
       }, []);
     const loadtodos = () => {
     setLoading(1); 
-    axios.get("http://127.0.0.1:5000/api/users/loadtodo", {
+    axios.get("https://merntodofrontend-rosy.vercel.app/api/users/loadtodo", {
       headers: {
         Authorization: `Bearer ${token}`, // Add the Authorization header with the token
       },
